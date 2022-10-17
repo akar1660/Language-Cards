c258427347061c23a5791fd5a2aacef150905e2f
Joseph <alisanakar1@gmail.com>
2022-10-17 01:28:07 -0400

    item css

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,8 +1,17 @@
-
-const Card = (data) => {
+import {languages} from '../../helpers/data'
+import Item from '../item/Item'
+const Card = () => {
   return (
-    <div>
-        {/* {data.map((item) => {...item})} */}
+    <div className='cards-area-container'>
+      <div className='bars'></div>
+      <h1 className='language-title'>Languages</h1>
+      <div className='cards-container'>
+        {languages.map((item, index) => {
+          return(
+            <Item card={item} key={index}/>
+          )
+        })}
+      </div> 
     </div>
   )
 }
